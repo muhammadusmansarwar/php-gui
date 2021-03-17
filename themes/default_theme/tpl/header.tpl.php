2558487937b720b35422759d2e0d936c7449dae6
Muhammad Usman Sarwar <muhammadusmanserwer@yahoo.com>
2021-03-18 01:15:55 +0500

    dddd

--- themes/default_theme/tpl/header.tpl.php
+++ themes/default_theme/tpl/header.tpl.php
@@ -42,12 +42,24 @@
             </div>
             <div class="col-12 col-md-9 col-lg-10" style="color: blue;">
                 <nav class="navbar navbar-expand-lg navbar-light bg-light w-100 p-0 m-0">
-                    <div class="container-fluid">
+                    <div class="container-fluid top_bar_link">
                         <div class="col-6">
                             <div class="collapse navbar-collapse" id="navbarCollapse">
                                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
                                     <li class="nav-item">
-                                        <a class="nav-link" href="#"><i class="fas fa-gift me-1"></i> Mega Menu<i class="icon icon-chevron-down"></i></a>
+                                        <div class="btn-group">
+                                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                                                Dropdown
+                                            </button>
+                                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                <li><a class="dropdown-item" href="#">Menu item</a></li>
+                                                <li><a class="dropdown-item" href="#">Menu item</a></li>
+                                                <li><a class="dropdown-item" href="#">Menu item</a></li>
+                                            </ul>
+                                        </div>
+                                        <!-- <a class="nav-link" href="#"><i class="fas fa-gift me-1"></i> Mega Menu<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                                                <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
+                                            </svg></a> -->
                                     </li>
                                     <li class="nav-item">
                                         <a class="nav-link" href="#">Link</a>
@@ -56,6 +68,7 @@
                                         <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
                                     </li>
                                 </ul>
+
                             </div>
                         </div>
                         <div class="col-6">
